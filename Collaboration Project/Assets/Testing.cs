473c4f648642bf4f782e8155f4b59843ef77cd1a
Aayush Arora <aayusharoralinkedin@gmail.com>
2025-05-19 17:13:55 +0530

    Cube Movement

---
+++ Collaboration Project/Assets/Testing.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, 0f, 5f);
+    }
+}
